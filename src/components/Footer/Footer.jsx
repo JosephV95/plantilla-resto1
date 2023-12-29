3a0b19350557daa5344111be285fec623c9ceae4
JosephV95 <viltejl@gmail.com>
2023-12-29 15:23:21 -0300

    Se desarrolla el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,14 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className="container mx-auto ">
+      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-6">
+        <p className=" h-14 bg-red-400">hola mundo</p>
+        <p className=" h-14 bg-blue-400">hola mundo 2</p>
+        <p className=" h-14 bg-green-400">hola mundo 3</p>
+        <p className=" h-14 bg-purple-400">hola mundo 4</p>
+        <p className=" h-14 bg-green-400">hola mundo 3</p>
+        <p className=" h-14 bg-blue-400">hola mundo 2</p>
+      </div>
+    </div>
   )
 }
