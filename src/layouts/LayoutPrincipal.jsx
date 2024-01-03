519ba480ce621431571625dbfc0c23821d20762d
JosephV95 <viltejl@gmail.com>
2024-01-03 20:57:37 -0300

    Se esta creando el navbar

--- src/layouts/LayoutPrincipal.jsx
+++ src/layouts/LayoutPrincipal.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Nav from "../components/Navbar/Nav";
 import Footer from "../components/Footer/Footer";
+import Nav from "../components/Navbar/Nav";
 
 export default function LayoutPrincipal() {
   return (
