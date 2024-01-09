a90ca9241839b894b4c073945c41b2d89e9c8a30
JosephV95 <viltejl@gmail.com>
2024-01-09 20:32:48 -0300

    Se esta editando el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <div className="container mx-auto ">
-      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4">
+      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-2">
         <p className=" h-screen bg-red-400">hola mundo</p>
         <p className=" h-80 bg-blue-400">hola mundo 2</p>
         <p className=" h-80 bg-green-400">hola mundo 3</p>
-        <p className=" h-80 bg-purple-400">hola mundo 4</p>
-        <p className=" h-80 bg-green-400">hola mundo 3</p>
-        <p className=" h-80 bg-blue-400">hola mundo 2</p>
+        <p className=" h-screen bg-purple-400">hola mundo 4</p>
+        <p className=" h-screen bg-green-400">hola mundo 3</p>
+        <p className=" h-screen bg-blue-400">hola mundo 2</p>
       </div>
     </div>
   )
