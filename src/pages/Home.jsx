40f1a590be97df6ab2153c9c692aa1911e9eae6a
JosephV95 <viltejl@gmail.com>
2024-01-11 13:33:10 -0300

    Se esta editando estilos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
-    <>
+    <div className="container max-w-full  bg-teal-50 pt-[5rem]">
       <Hero />
-    </>
+    </div>
   );
 }
