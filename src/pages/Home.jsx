263d39603486ec86153a839ab9d66112323ad85d
JosephV95 <viltejl@gmail.com>
2024-01-11 14:19:39 -0300

    Se esta creando un slider en el header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import CarouselDelHeader from "../components/Carousel/carouselDelHeader";
 import Hero from "../components/Hero/Hero";
 
 export default function Home() {
   return (
     <div className="container max-w-full bg-teal-50 pt-[5rem]">
+      <CarouselDelHeader />
       <Hero />
     </div>
   );
