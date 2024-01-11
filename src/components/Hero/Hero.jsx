70d7cf3ac10bc6f81bef6067cc4b7dac6870f1d8
JosephV95 <viltejl@gmail.com>
2024-01-11 13:09:17 -0300

    Se edita el nav

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,8 @@
+
+export default function Hero() {
+  return (
+    <div className="container max-w-full h-screen bg-teal-50 pt-20">
+        Hola
+    </div>
+  )
+}
--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -63,7 +63,7 @@ export default function Nav() {
   );
 
   return (
-    <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+    <Navbar className=" top-0 z-10 h-max max-w-full absolute rounded-b-xl rounded-t-none border-none px-4 py-2 lg:px-8 lg:py-4 bg-gradient-to-r from-teal-300 to-green-200 ">
       <div className="flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
