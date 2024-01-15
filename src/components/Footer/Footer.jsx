f6463a939dba64dfb41ee8805dc2dec9b3f778a8
JosephV95 <viltejl@gmail.com>
2024-01-14 23:13:00 -0300

    Se edita el footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,23 @@
 export default function Footer() {
   return (
-    <div className="container mx-auto ">
-      <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-2">
+    <div className="container mx-auto bg-black/80">
+      <div className="text-white text-center">
+        <h1>Delicious</h1>
+        <p>Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
+
+        <p>© Copyright Delicious. All Rights Reserved</p>
+        <p>Creado por José Vilte</p>
+      </div>
+      
+      
+      {/* <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-2">
         <p className=" h-screen bg-red-400">hola mundo</p>
         <p className=" h-80 bg-blue-400">hola mundo 2</p>
         <p className=" h-80 bg-green-400">hola mundo 3</p>
         <p className=" h-screen bg-purple-400">hola mundo 4</p>
         <p className=" h-screen bg-green-400">hola mundo 3</p>
         <p className=" h-screen bg-blue-400">hola mundo 2</p>
-      </div>
+      </div> */}
     </div>
   )
 }
