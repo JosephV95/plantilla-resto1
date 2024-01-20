01fcc4d1edd7e954a9e1a2d251a142c82fa2ec9e
JosephV95 <viltejl@gmail.com>
2024-01-20 13:17:55 -0300

    Se empezo el About

---
+++ src/components/SobreNosotros/SobreNosotros.jsx
@@ -0,0 +1,37 @@
+export default function SobreNosotros() {
+  return (
+    <section className="w-full flex relative columns-1">
+      <div className="w-5/12 bg-orange-400">
+        <img src="../img/about.jpg" alt="imgAbout" className="object-cover" />
+      </div>
+      <div className="w-7/12  bg-cyan-500 px-12">
+        <h1>Eum ipsam laborum deleniti velit pariatur architecto aut nihil</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute
+          irure dolor in reprehenderit
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+        <ul>
+          <li>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+          <li>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+          <li>
+            Ullamco laboris nisi ut aliquip ex ea commodo consequat.Ullamco
+            laboris nisi ut aliquip ex ea commodo consequat. Ullamco laboris
+            nisi ut aliquip ex ea commodo consequat.
+          </li>
+        </ul>
+
+        <p>
+          Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import CarouselDelHeader from "../components/Carousel/CarouselDelHeader";
 import Hero from "../components/Hero/Hero";
+import SobreNosotros from "../components/SobreNosotros/SobreNosotros";
 
 export default function Home() {
   return (
     <div className="container max-w-full bg-teal-50 ">
       <CarouselDelHeader />
       <Hero />
+      <SobreNosotros />
+      
     </div>
   );
 }
