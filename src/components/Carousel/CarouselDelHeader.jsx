83145db17b8c39792b7e66569833a63302fee584
JosephV95 <viltejl@gmail.com>
2024-01-20 12:47:39 -0300

    Se mejoro estilo del nav

--- src/components/Carousel/CarouselDelHeader.jsx
+++ src/components/Carousel/CarouselDelHeader.jsx
@@ -5,7 +5,7 @@ import CallToAction from "../CallToAction/CallToAction";
 export default function CarouselDelHeader() {
   return (
     <Carousel
-      className=" h-[99vh] relative" id="miSlider" autoplay loop
+      className=" h-[100vh] relative" id="miSlider" autoplay loop
       navigation={({ setActiveIndex, activeIndex, length }) => (
         <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
           {new Array(length).fill("").map((_, i) => (
@@ -23,7 +23,7 @@ export default function CarouselDelHeader() {
       <div className="h-full w-full  bg-[url('../img/slide/slide-1.jpg')] bg-cover bg-top "> 
         <div className="h-full w-full absolute bg-black/40 flex justify-center">
           <CallToAction 
-          tituloEnHTML={<h2 className="text-2xl" style={{color:"white"}}><span style={{color:"orange"}}>Delicious</span> Restaurante</h2>}  
+          tituloEnHTML={<h2 className="text-2xl md:text-4xl" style={{color:"white"}}><span style={{color:"orange"}}>Delicious</span> Restaurante</h2>}  
           texto={'Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.'}
           />
         </div>
@@ -32,7 +32,7 @@ export default function CarouselDelHeader() {
       <div className="h-full w-full  bg-[url('../img/slide/slide-2.jpg')] bg-cover bg-top "> 
         <div className="h-full w-full absolute bg-black/40 flex justify-center">
           <CallToAction 
-          tituloEnHTML={<h2 className="text-2xl">Miraculous Restaurant</h2>}  
+          tituloEnHTML={<h2 className="text-2xl md:text-4xl">Miraculous Restaurant</h2>}  
           texto={'Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.'}
           />
         </div>
@@ -40,7 +40,7 @@ export default function CarouselDelHeader() {
       <div className="h-full w-full  bg-[url('../img/slide/slide-3.jpg')] bg-cover bg-top "> 
         <div className="h-full w-full absolute bg-black/40 flex justify-center">
           <CallToAction 
-          tituloEnHTML={<h2 className="text-2xl">Miraculous Restaurant</h2>}  
+          tituloEnHTML={<h2 className="text-2xl md:text-4xl">Miraculous Restaurant</h2>}  
           texto={'Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.'}
           />
         </div>
