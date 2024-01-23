73eeb5222a90cf6f84f25131ba4c1903e827f299
JosephV95 <viltejl@gmail.com>
2024-01-23 00:05:50 -0300

    Se edito el About

--- src/components/SobreNosotros/SobreNosotros.jsx
+++ src/components/SobreNosotros/SobreNosotros.jsx
@@ -1,7 +1,9 @@
+import { Card, CardBody, CardHeader, Typography } from "@material-tailwind/react";
+
 export default function SobreNosotros() {
   return (
     <section className="w-full h-auto flex py-16 columns-1 bg-orange-50">
-      <div className="w-[100vw] md:w-5/12  bg-orange-400 ">
+      {/* <div className="w-[100vw] md:w-5/12  bg-orange-400 ">
         <img src="../img/about.jpg" alt="imgAbout" className="object-cover" />
       </div>
       <div className="w-[100vw] md:w-7/12  bg-cyan-500 px-4 md:x-24 space-y-4 my-auto py-auto">
@@ -31,7 +33,36 @@ export default function SobreNosotros() {
           fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
           sunt in culpa qui officia deserunt mollit anim id est laborum
         </p>
-      </div>
+      </div> */}
+
+      <Card className="w-full  flex-row flex-wrap">
+            <CardHeader
+              shadow={false}
+              floated={false}
+              className="m-0 w-full md:w-2/5 shrink-0 rounded-r-none"
+            >
+              <img
+                src="../img/about.jpg"
+                alt="card-image"
+                className="h-full w-full object-cover"
+              />
+            </CardHeader>
+            <CardBody className="w-full md:w-3/5">
+              <Typography variant="h6" color="gray" className="mb-4 uppercase">
+                startups
+              </Typography>
+              <Typography variant="h4" color="blue-gray" className="mb-2">
+                Lyft launching cross-platform service this week
+              </Typography>
+              <Typography color="gray" className="mb-8 font-normal">
+                Like so many organizations these days, Autodesk is a company in
+                transition. It was until recently a traditional boxed software company
+                selling licenses. Yet its own business model disruption is only part
+                of the story
+              </Typography>
+             
+            </CardBody>
+          </Card>
     </section>
   );
 }
