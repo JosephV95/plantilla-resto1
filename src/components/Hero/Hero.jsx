56e6c3bd312eaea47bdad7e5b73352c8b36b31bf
JosephV95 <viltejl@gmail.com>
2024-01-30 19:44:37 -0300

    Se esta editando contenido del Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,25 +10,25 @@ export default function Hero() {
       {/* </div> */}
 
       <div className="flex flex-row flex-wrap gap-6 justify-center">
-        <div className="cardHero w-3/12">
-          <h2>01</h2>
-          <h2>Lorem Impsum</h2>
+        <div className="cardHero w-3/12 space-y-5">
+          <h2 className="text-3xl font-semibold cardNumero">01</h2>
+          <h2 className="text-2xl font-bold">Lorem Impsum</h2>
           <p>
             Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et
             consectetur ducimus vero placeat
           </p>
         </div>
-        <div className="cardHero w-3/12">
-          <h2>02</h2>
-          <h2>Repellat Nihil</h2>
+        <div className="cardHero w-3/12 space-y-5">
+          <h2 className="text-3xl font-semibold cardNumero">02</h2>
+          <h2 className="text-2xl font-bold">Repellat Nihil</h2>
           <p>
             Molestiae officiis omnis illo asperiores. Aut doloribus vitae sunt
             debitis quo vel nam quis
           </p>
         </div>
-        <div className="cardHero w-3/12">
-          <h2>03</h2>
-          <h2>Faker</h2>
+        <div className="cardHero w-3/12 space-y-5">
+          <h2 className="text-3xl font-semibold cardNumero">03</h2>
+          <h2 className="text-2xl font-bold">Faker</h2>
           <p>
             Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et
             consectetur ducimus vero placeat
