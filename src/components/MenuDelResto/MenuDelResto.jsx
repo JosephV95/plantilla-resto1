38d924124a1b26a88686ff69ee82522038f24c52
JosephV95 <viltejl@gmail.com>
2024-01-31 14:30:34 -0300

    Se comenzo la seccion de menu del resto

---
+++ src/components/MenuDelResto/MenuDelResto.jsx
@@ -0,0 +1,19 @@
+
+export default function MenuDelResto() {
+  return (
+    <section className="w-full min-h-[50vh] bg-blue-gray-200">
+        <h1 className="text-center">Consulta nuestro <span className="text-[#ffb03b]">sabroso Menú</span></h1>
+
+        <div>
+            <ul>
+                <li>Todas</li>
+                <li>Saladas</li>
+                <li>Especiales</li>
+                <li>Entradas</li>
+            </ul>
+        </div>
+
+
+    </section>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import CarouselDelHeader from "../components/Carousel/CarouselDelHeader";
 import Hero from "../components/Hero/Hero";
+import MenuDelResto from "../components/MenuDelResto/MenuDelResto";
 import SobreNosotros from "../components/SobreNosotros/SobreNosotros";
 
 export default function Home() {
@@ -8,7 +9,7 @@ export default function Home() {
       <CarouselDelHeader />
       <SobreNosotros />
       <Hero />
-      
+      <MenuDelResto />
     </div>
   );
 }
