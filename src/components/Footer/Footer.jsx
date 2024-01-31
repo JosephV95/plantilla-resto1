d05ba083e095e9e9c650e3c74300c533d2936d45
JosephV95 <viltejl@gmail.com>
2024-01-31 19:08:01 -0300

    Se soluciono un error que aparecia en consola

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,10 +6,10 @@ export default function Footer() {
         <p className="text-">Et aut eum quis fuga eos sunt ipsa nihil. Labore corporis magni eligendi fuga maxime saepe commodi placeat.</p>
 
         <div className="redesLinks flex flex-row gap-4 justify-center">
-          <a href="https://es-la.facebook.com"  target="_blank" rel="noopener noreferrer" ><box-icon name='facebook' type='logo' color='#f8f8f8' size='md'></box-icon></a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" ><box-icon name='instagram' type='logo' color='#f8f8f8' size='md'></box-icon></a>
-          <a href="" target="_blank" rel="noopener noreferrer" ><box-icon name='whatsapp' type='logo' color='#f8f8f8' size='md'></box-icon></a>
-          <a href="https://twitter.com/?lang=es" target="_blank" rel="noopener noreferrer" ><box-icon name='twitter' type='logo' color='#f8f8f8' size='md'></box-icon></a>
+          <a href="https://es-la.facebook.com"  target="_blank" rel="noopener noreferrer" ><box-icon type='logo' name='facebook' color='#f8f8f8' size='md'></box-icon></a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer" ><box-icon type='logo' name='instagram' color='#f8f8f8' size='md'></box-icon></a>
+          <a href="" target="_blank" rel="noopener noreferrer" ><box-icon type='logo' name='whatsapp' color='#f8f8f8' size='md'></box-icon></a>
+          <a href="https://twitter.com/?lang=es" target="_blank" rel="noopener noreferrer" ><box-icon type='logo' name='twitter' color='#f8f8f8' size='md'></box-icon></a>
         </div>
 
         <p>© Copyright Delicious. All Rights Reserved</p>
