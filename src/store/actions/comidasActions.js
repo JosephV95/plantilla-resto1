dab1e0e40b532b3b2d690e171e47abab4395456e
JosephV95 <viltejl@gmail.com>
2024-02-03 22:56:06 -0300

    Se logro el funcionamiento del action

--- src/store/actions/comidasActions.js
+++ src/store/actions/comidasActions.js
@@ -2,6 +2,7 @@ import { createAction } from "@reduxjs/toolkit";
 
 const obtener_comidas = createAction("obtenerComidas", (payload) => {
   // console.log(payload);
+  //! SE DEBE DESCOMPONER EL PAYLOAD y retornar {payload}
   return {payload};
 });
 
