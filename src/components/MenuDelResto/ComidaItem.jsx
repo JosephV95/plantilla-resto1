a9f43eb1aa7727b7d5e72d3e6bd9e636066f4ce3
JosephV95 <viltejl@gmail.com>
2024-02-05 17:57:03 -0300

    Se mejoro estilos para las comidas en el menú

--- src/components/MenuDelResto/ComidaItem.jsx
+++ src/components/MenuDelResto/ComidaItem.jsx
@@ -2,16 +2,16 @@
 
 export default function ComidaItem({nombre, img, descripcion, precio}) {
   return (
-    <div className="w-[50%] ">
-        <div >
+    <div className="w-[45%] flex flex-row">
+        <div className=" relative">
             <img src={img} alt={nombre} className="w-20 h-20 object-cover rounded-full" />
         </div>
-        <div>
-            <h3>{nombre}</h3>
+        <div className="w-10/12 px-3">
+            <h3 className="text-[#ffb03b] font-semibold">{nombre}</h3>
             <p>{descripcion}</p>
         </div>
-        <div>
-            <h3>{precio}</h3>
+        <div className="w-1/12">
+            <h3 className="font-bold ">${precio}</h3>
         </div>
     </div>
   )
