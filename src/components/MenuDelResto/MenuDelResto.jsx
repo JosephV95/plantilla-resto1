4036ca277e577483e1eb6d1000d5f9d2ede0f90c
JosephV95 <viltejl@gmail.com>
2024-02-08 20:01:02 -0300

    Se esta probando funcionamiento de la libreria isotope

--- src/components/MenuDelResto/MenuDelResto.jsx
+++ src/components/MenuDelResto/MenuDelResto.jsx
@@ -1,32 +1,74 @@
-import { useSelector } from "react-redux"
-import ComidaItem from "./ComidaItem"
+import { useState, useEffect, useRef } from "react";
+import Isotope from "isotope-layout";
+import { useSelector } from "react-redux";
 
 export default function MenuDelResto() {
+  const [filter, setFilter] = useState('*');
+  const gridRef = useRef(null);
+  let iso;
   const comidasStore = useSelector(store => store.comidasReducer.comidas)
   // console.log(comidasStore);
 
+  useEffect(() => {
+    iso = new Isotope(gridRef.current, {
+      itemSelector: '.grid-item',
+      layoutMode: 'fitRows',
+    });
+
+    return () => {
+      iso.destroy();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (iso) {
+      iso.arrange({ filter });
+    }
+  }, [filter]);
+
+  const handleFilter = (newFilter) => {
+    console.log(newFilter);
+    setFilter(newFilter);
+  };
+
   return (
-    <section className="w-full min-h-[50vh] py-16 space-y-5">
-        <h1 className="text-center text-3xl">Consulta nuestro <span className="text-[#ffb03b]">sabroso Menú</span></h1>
+    // <section className="w-full min-h-[50vh] py-16 space-y-5">
+    //   <h1 className="text-center text-3xl">Consulta nuestro <span className="text-[#ffb03b]">sabroso Menú</span></h1>
 
-        <div>
-            <ul className="flex flex-row flex-wrap justify-center gap-x-3 gap-y-2">
-                <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline ">Todas</li>
-                <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline ">Saladas</li>
-                <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline ">Especiales</li>
-                <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline ">Entradas</li>
-            </ul>
-        </div>
+    //   <div>
+    //     <ul className="flex flex-row flex-wrap justify-center gap-x-3 gap-y-2">
+    //       <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline " onClick={() => filterItems('*')}>Todas</li>
+    //       <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline " onClick={() => filterItems('.carnes')}>Carnes</li>
+    //       <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline ">Especiales</li>
+    //       <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline ">Entradas</li>
+    //     </ul>
+    //   </div>
 
-        {/* <div className="w-full columns-2 flex flex-row flex-wrap justify-between px-24 gap-y-3"> */}
-          <div className="w-full py-3 flex flex-row flex-wrap justify-center lg:justify-between px-2 sm:px-8 lg:px-24 gap-y-5">
-            {comidasStore.slice(0, 9).map((comida, key)=>(
-              <ComidaItem key={key} nombre={comida.nombre} img={comida.imgUrl} descripcion={comida.descripcion} precio={comida.precio}></ComidaItem>
-            ))
-            }
+    //   <div className="grid" ref={gridRef}>
+    //     <div className="w-full py-3 flex flex-row flex-wrap justify-center lg:justify-between px-2 sm:px-8 lg:px-24 gap-y-5">
+    //       {comidasStore.slice(0, 9).map((comida, key) => (
+    //         <ComidaItem key={key} nombre={comida.nombre} className={`grid-item ${comida.categoria}`} img={comida.imgUrl} descripcion={comida.descripcion} precio={comida.precio}></ComidaItem>
+    //       ))}
+    //     </div>
+    //   </div>
+    // </section>
+    <div>
+      <div>
+        <button onClick={() => handleFilter('*')}>Todas</button>
+        <button onClick={() => handleFilter('.carnes')}>Carnes</button>
+        <button onClick={() => handleFilter('.pastas')}>Pastas</button>
+        <button onClick={() => handleFilter('.ensaladas')}>Ensaladas</button>
+        <button onClick={() => handleFilter('.pizzas')}>Pizzas</button>
+        <button onClick={() => handleFilter('.sushi')}>Sushi</button>
+        <button onClick={() => handleFilter('.tacos')}>Tacos</button>
+      </div>
+      <div ref={gridRef} className="grid">
+        {comidasStore.map((comida, index) => (
+          <div key={index} className={`grid-item ${comida.categoria} bg-green-300 rounded-full mx-3 inline`}>
+            {comida.nombre}
           </div>
-        {/* </div> */}
-
-    </section>
+        ))}
+      </div>
+    </div>
   )
 }
