f202cb842c41a5e1320d86fd9b289bd2bb6b731b
JosephV95 <viltejl@gmail.com>
2024-02-12 23:21:03 -0300

    Se esta arreglando tamaños de los divs

--- src/components/MenuDelResto/ComidaItem.jsx
+++ src/components/MenuDelResto/ComidaItem.jsx
@@ -2,13 +2,13 @@
 
 export default function ComidaItem({nombre, img, descripcion, precio, categoria}) {
   return (
-    <div className={`w-[100%] lg:w-[49%] flex flex-row justify-center  filtr-item`} data-category={categoria}>
+    <div className={`w-[100%] lg:w-[40%] flex flex-row justify-center   filtr-item`} data-category={categoria}>
         <div className="">
             <img src={img} alt={nombre} className="w-14 sm:w-20   object-cover rounded-full" />
         </div>
         <div className="w-10/12 px-3">
             <h3 className="text-[#ff9900] font-extrabold border-dotted border-b-2 border-red-300">{nombre}</h3>
-            <p>{descripcion}</p>
+            <p className="text-sm">{descripcion}</p>
         </div>
         <div className="">
             <h3 className="font-bold ">${precio}</h3>
