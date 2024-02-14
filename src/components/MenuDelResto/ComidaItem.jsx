269293210edc6f84d04b18126e69a339688c2596
JosephV95 <viltejl@gmail.com>
2024-02-13 22:41:45 -0300

    Se edito el tamaño de los div

--- src/components/MenuDelResto/ComidaItem.jsx
+++ src/components/MenuDelResto/ComidaItem.jsx
@@ -2,15 +2,15 @@
 
 export default function ComidaItem({nombre, img, descripcion, precio, categoria}) {
   return (
-    <div className={`w-[100%] lg:w-[40%] flex flex-row justify-center   filtr-item`} data-category={categoria}>
-        <div className="">
+    <div className={`w-[100%] lg:w-[45%] flex flex-row justify-center   filtr-item`} data-category={categoria}>
+        <div className="1/12">
             <img src={img} alt={nombre} className="w-14 sm:w-20   object-cover rounded-full" />
         </div>
-        <div className="w-10/12 px-3">
+        <div className="w-9/12 ps-3">
             <h3 className="text-[#ff9900] font-extrabold border-dotted border-b-2 border-red-300">{nombre}</h3>
-            <p className="text-sm">{descripcion}</p>
+            <p className="text-xs">{descripcion}</p>
         </div>
-        <div className="">
+        <div className="2/12">
             <h3 className="font-bold ">${precio}</h3>
         </div>
     </div>
