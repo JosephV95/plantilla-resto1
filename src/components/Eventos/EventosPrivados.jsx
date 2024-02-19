323fb37bf20f5d5fc97e5f1b536786729b4a3179
JosephV95 <viltejl@gmail.com>
2024-02-19 19:47:00 -0300

    se corrigio funcionalidad del carousel

--- src/components/Eventos/EventosPrivados.jsx
+++ src/components/Eventos/EventosPrivados.jsx
@@ -6,7 +6,7 @@ export default function EventosPrivados() {
       <article className="w-full relative bg-[url('../img/events/events-bg.jpg')] bg-cover bg-center py-12 flex flex-col items-center" id="carouselEnEventos">
 
         <h2 className="z-20 text-white text-3xl font-bold text-center">Organiza <span className="text-orange-400">tus eventos</span> con nosotros</h2>
-        <Carousel className="z-20 " loop  prevArrow={false} autoplay autoplayDelay={7000}
+        <Carousel className="z-20 " loop  prevArrow={false} nextArrow={false} autoplay autoplayDelay={7000}
 
         //! navigation es un atributo para personalizar los botones inferiores del carousel (es propio de Material-tailwind) 
         navigation={({ setActiveIndex, activeIndex, length }) => (
@@ -46,7 +46,7 @@ export default function EventosPrivados() {
               <img src="../img/events/event-private.jpg" alt="imgEventos" className="h-full w-full object-cover" />
             </div>
             <div className="w-11/12 sm:w-4/5 lg:w-5/12 text-white space-y-4">
-              <h3 className="text-2xl text-orange-300 font-semibold">Fiestas Personalizadas</h3>
+              <h3 className="text-2xl text-orange-300 font-semibold">Fiestas Privadas</h3>
               <h3 className="text-2xl font-semibold underline underline-offset-8 decoration-orange-300">$3000</h3>
               <p className="italic text-[1.1rem]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus architecto dolorem praesentium molestiae amet, quia optio accusantium ab adipisci quidem, aut quam expedita eum porro deserunt libero consectetur! Totam, officia.</p>
 
@@ -65,7 +65,7 @@ export default function EventosPrivados() {
               <img src="../img/events/event-birthday.jpg" alt="imgEventos" className="h-full w-full object-cover" />
             </div>
             <div className="w-11/12 sm:w-4/5 lg:w-5/12 text-white space-y-4">
-              <h3 className="text-2xl text-orange-300 font-semibold">Fiestas Personalizadas</h3>
+              <h3 className="text-2xl text-orange-300 font-semibold">Fiestas de Cumplaños</h3>
               <h3 className="text-2xl font-semibold underline underline-offset-8 decoration-orange-300">$3000</h3>
               <p className="italic text-[1.1rem]">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus architecto dolorem praesentium molestiae amet, quia optio accusantium ab adipisci quidem, aut quam expedita eum porro deserunt libero consectetur! Totam, officia.</p>
               <ul className="ps-6 space-y-2">
