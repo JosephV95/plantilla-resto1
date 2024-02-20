07f47b0b148d6e90cc88053c1efdd8e2da335546
JosephV95 <viltejl@gmail.com>
2024-02-20 13:52:12 -0300

    Se agrego efecto hover a las imagenes

--- src/components/GalleriaImg/GalleriaItem.jsx
+++ src/components/GalleriaImg/GalleriaItem.jsx
@@ -3,7 +3,7 @@
 
 export default function GalleriaItem({urlImg}) {
   return (
-    <div className="w-full mb-1 overflow-hidden ">
+    <div className="w-full mb-0.5 md:mb-1 overflow-hidden">
         <a >
             <img src={urlImg} alt={'Img' + urlImg} className="w-full h-full object-cover duration-1000 hover:scale-[1.15]"/>
         </a>
