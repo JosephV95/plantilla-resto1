9b75e2c6df424b882a804a364ac9580ec7f00bc6
JosephV95 <viltejl@gmail.com>
2024-02-20 20:02:40 -0300

    Se esta configurando fslightbox react

--- src/components/GalleriaImg/GalleriaImg.jsx
+++ src/components/GalleriaImg/GalleriaImg.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import GalleriaItem from "./GalleriaItem";
 
+import FsLightbox from "fslightbox-react";
+
 export default function GalleriaImg() {
+  const [toggler, setToggler] = useState(false);
+
+
   return (
-    <section className="text-center flex flex-col items-center gap-y-4  my-8 py-3">
+    <section className="text-center flex flex-col items-center gap-y-4   my-8 py-3">
       <h1 className="text-3xl font-semibold text-[#5f5950]">Algunas fotos de <span className="text-[#ffb03b]">Nuestro Restaurante</span></h1>
       <p className="w-4/5 md:w-3/5 text-lg text-[#68645e]">Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
 
       <article className="sm:columns-2 md:columns-3 lg:columns-4 gap-1 px-4 sm:px-3">
+      <FsLightbox
+				toggler={toggler}
+				sources={[
+					'../img/gallery/gallery-1.jpg',
+          '../img/gallery/gallery-2.jpg',
+          '../img/gallery/gallery-3.jpg',
+          '../img/gallery/gallery-4.jpg',
+          '../img/gallery/gallery-5.jpg',
+          '../img/gallery/gallery-6.jpg',
+          '../img/gallery/gallery-7.jpg',
+          '../img/gallery/gallery-8.jpg'
+				]}
+			/>
+      <button onClick={() => setToggler(!toggler)}>
+				Toggle Lightbox
+			</button>
         <GalleriaItem urlImg={'../img/gallery/gallery-1.jpg'} />
         <GalleriaItem urlImg={'../img/gallery/gallery-2.jpg'} />
         <GalleriaItem urlImg={'../img/gallery/gallery-3.jpg'} />
