5c714a2f10fcac5693868bc6481ecc1a8a092def
JosephV95 <viltejl@gmail.com>
2024-02-20 23:44:55 -0300

    Se esta configurando fslightbox react

--- src/components/GalleriaImg/GalleriaImg.jsx
+++ src/components/GalleriaImg/GalleriaImg.jsx
@@ -6,6 +6,18 @@ import FsLightbox from "fslightbox-react";
 export default function GalleriaImg() {
   const [toggler, setToggler] = useState(false);
 
+  const [lightboxController, setLightboxController] = useState({
+		toggler: false,
+		slide: 1
+	});
+
+  function openLightboxOnSlide(number) {
+		setLightboxController({
+			toggler: !lightboxController.toggler,
+			slide: number
+		});
+	}
+
 
   return (
     <section className="text-center flex flex-col items-center gap-y-4   my-8 py-3">
@@ -14,7 +26,9 @@ export default function GalleriaImg() {
 
       <article className="sm:columns-2 md:columns-3 lg:columns-4 gap-1 px-4 sm:px-3">
       <FsLightbox
-				toggler={toggler}
+        toggler={lightboxController.toggler}
+				
+				slide={lightboxController.slide}
 				sources={[
 					'../img/gallery/gallery-1.jpg',
           '../img/gallery/gallery-2.jpg',
@@ -26,12 +40,11 @@ export default function GalleriaImg() {
           '../img/gallery/gallery-8.jpg'
 				]}
 			/>
-      <button onClick={() => setToggler(!toggler)}>
-				Toggle Lightbox
-			</button>
-        <GalleriaItem urlImg={'../img/gallery/gallery-1.jpg'} />
-        <GalleriaItem urlImg={'../img/gallery/gallery-2.jpg'} />
-        <GalleriaItem urlImg={'../img/gallery/gallery-3.jpg'} />
+       <button onClick={()=>{openLightboxOnSlide(1)}}>hol1</button>
+       <button onClick={()=>{openLightboxOnSlide(5)}}>hol4</button>
+        <GalleriaItem urlImg={'../img/gallery/gallery-1.jpg'} onClick={()=>{openLightboxOnSlide(1)}}/>
+        <GalleriaItem urlImg={'../img/gallery/gallery-2.jpg'} onClick={()=>{openLightboxOnSlide(2)}}/>
+        <GalleriaItem urlImg={'../img/gallery/gallery-3.jpg'} onClick={()=>{openLightboxOnSlide(3)}}/>
         <GalleriaItem urlImg={'../img/gallery/gallery-4.jpg'} />
         <GalleriaItem urlImg={'../img/gallery/gallery-5.jpg'} />
         <GalleriaItem urlImg={'../img/gallery/gallery-6.jpg'} />
