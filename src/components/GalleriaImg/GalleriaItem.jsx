1860fe08a06e6afe8d86d3fff150c8bd23f516df
JosephV95 <viltejl@gmail.com>
2024-02-21 00:00:47 -0300

    Se agrego Prop para el funcionamiento del visor de imagenes

--- src/components/GalleriaImg/GalleriaItem.jsx
+++ src/components/GalleriaImg/GalleriaItem.jsx
@@ -1,13 +1,11 @@
-
 /* eslint-disable react/prop-types */
 
-export default function GalleriaItem({urlImg}) {
+export default function GalleriaItem({ urlImg, onClickCreado }) {
   return (
-    <div className="w-full mb-0.5 md:mb-1 overflow-hidden">
-        <a >
-            <img src={urlImg} alt={'Img' + urlImg} className="w-full h-full object-cover duration-1000 hover:scale-[1.15]"/>
-        </a>
+    <div className="w-full mb-0.5 md:mb-1 overflow-hidden" onClick={onClickCreado}>
+      
+      <img src={urlImg} alt={"Img" + urlImg} className="w-full h-full object-cover duration-1000 hover:scale-[1.15]" />
 
     </div>
-  )
+  );
 }
