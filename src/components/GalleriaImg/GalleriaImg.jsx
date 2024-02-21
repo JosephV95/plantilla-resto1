16c3eee900a216a883aac7fb390be5b9760c23c5
JosephV95 <viltejl@gmail.com>
2024-02-21 20:05:23 -0300

    Se idento el codigo

--- src/components/GalleriaImg/GalleriaImg.jsx
+++ src/components/GalleriaImg/GalleriaImg.jsx
@@ -23,20 +23,20 @@ export default function GalleriaImg() {
       <p className="w-4/5 md:w-3/5 text-lg text-[#68645e]">Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
 
       <article className="sm:columns-2 md:columns-3 lg:columns-4 gap-1 px-4 sm:px-3">
-      <FsLightbox
-        toggler={lightboxController.toggler}
-				sources={[
-					'../img/gallery/gallery-1.jpg',
-          '../img/gallery/gallery-2.jpg',
-          '../img/gallery/gallery-3.jpg',
-          '../img/gallery/gallery-4.jpg',
-          '../img/gallery/gallery-5.jpg',
-          '../img/gallery/gallery-6.jpg',
-          '../img/gallery/gallery-7.jpg',
-          '../img/gallery/gallery-8.jpg'
-				]}
-        slide={lightboxController.slide}
-			/>
+        <FsLightbox
+          toggler={lightboxController.toggler}
+          sources={[
+            '../img/gallery/gallery-1.jpg',
+            '../img/gallery/gallery-2.jpg',
+            '../img/gallery/gallery-3.jpg',
+            '../img/gallery/gallery-4.jpg',
+            '../img/gallery/gallery-5.jpg',
+            '../img/gallery/gallery-6.jpg',
+            '../img/gallery/gallery-7.jpg',
+            '../img/gallery/gallery-8.jpg'
+          ]}
+          slide={lightboxController.slide}
+        />
         <GalleriaItem urlImg={'../img/gallery/gallery-1.jpg'}  onClickCreado={()=>{openLightboxOnSlide(1)}}/>
         <GalleriaItem urlImg={'../img/gallery/gallery-2.jpg'}  onClickCreado={()=>{openLightboxOnSlide(2)}}/>
         <GalleriaItem urlImg={'../img/gallery/gallery-3.jpg'}  onClickCreado={()=>{openLightboxOnSlide(3)}}/>
