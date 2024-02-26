b90b5ee0168633dc49f1a056ad59b3c25c73f037
JosephV95 <viltejl@gmail.com>
2024-02-26 20:05:27 -0300

    Se esta editando el contenido en Contacto

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -9,11 +9,11 @@ export default function Contacto() {
       <article className="w-4/5">
         <div className="flex flex-wrap">
           <div className="w-[25%]">
-            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-orange-200">
-              <box-icon name="time-five"></box-icon>
+            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8]">
+            <box-icon name='map' color='#ffb03b'></box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
-              Location:
+              Ubicación:
             </h4>
             <p className="ps-12 text-[#68645e]">
               info@example.com
@@ -22,11 +22,11 @@ export default function Contacto() {
             </p>
           </div>
           <div className="w-[25%]">
-            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-orange-200">
-              <box-icon name="time-five"></box-icon>
+            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8]">
+              <box-icon name="time-five" color='#ffb03b'></box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
-              Location:
+              Horarios:
             </h4>
             <p className="ps-12 text-[#68645e]">
               info@example.com
@@ -35,11 +35,11 @@ export default function Contacto() {
             </p>
           </div>
           <div className="w-[25%]">
-            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-orange-200">
-              <box-icon name="time-five"></box-icon>
+            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8]">
+              <box-icon name="envelope" color='#ffb03b'></box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
-              Location:
+              Email:
             </h4>
             <p className="ps-12 text-[#68645e]">
               info@example.com
@@ -48,11 +48,11 @@ export default function Contacto() {
             </p>
           </div>
           <div className="w-[25%]">
-            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-orange-200">
-              <box-icon name="time-five"></box-icon>
+            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8]">
+              <box-icon name="phone" color='#ffb03b'></box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
-              Location:
+              Llamanos:
             </h4>
             <p className="ps-12 text-[#68645e]">
               info@example.com
