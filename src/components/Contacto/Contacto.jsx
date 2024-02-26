921edf4b33ee7ba1f4081daf191faa05d6fbc144
JosephV95 <viltejl@gmail.com>
2024-02-25 23:54:17 -0300

    Se empezo a crear el componente Contacto

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -6,8 +6,47 @@ export default function Contacto() {
       <p>Encontranos en nuestro local o contactanos con un mensaje</p>
 
       <article>
+        <div>
+          <div className="w-[25%]">
+            <i></i>
+            <h4>Location:</h4>
+            <p>
+
+              <br />
+
+            </p>
+          </div>
+          <div className="w-[25%]">
+            <i></i>
+            <h4>Location:</h4>
+            <p>
+
+              <br />
+
+            </p>
+          </div>
+          <div className="w-[25%]">
+            <i></i>
+            <h4>Location:</h4>
+            <p>
+
+              <br />
+
+            </p>
+          </div>
+          <div className="w-[25%]">
+            <i></i>
+            <h4>Location:</h4>
+            <p>
+
+              <br />
+
+            </p>
+          </div>
+        </div>
+
         <form action="">
-          
+
         </form>
       </article>
     </section>
