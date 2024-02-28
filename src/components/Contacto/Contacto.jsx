91fa948658a0698aab408c2a86a1b778497a8bda
JosephV95 <viltejl@gmail.com>
2024-02-28 18:16:49 -0300

    Se esta arreglando el efecto hover en los iconos

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -2,14 +2,25 @@ import { useState } from "react";
 
 export default function Contacto() {
 
-  const [isHovered, setIsHovered] = useState(false);
+  const [hoverStates, setHoverStates] = useState({
+    ubicacion: false,
+    horarios: false,
+    email: false,
+    llamanos: false
+  });
 
-  const handleMouseEnter = () => {
-    setIsHovered(true);
+  const handleMouseEnter = (key) => {
+    setHoverStates((prev) => ({
+      ...prev,
+      [key]: true
+    }));
   };
 
-  const handleMouseLeave = () => {
-    setIsHovered(false);
+  const handleMouseLeave = (key) => {
+    setHoverStates((prev) => ({
+      ...prev,
+      [key]: false
+    }));
   };
 
   return (
@@ -21,9 +32,17 @@ export default function Contacto() {
 
       <article className="w-4/5">
         <div className="flex flex-wrap  ">
-          <div className="w-[25%] group" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div
+            className="w-[25%] group"
+            onMouseEnter={() => handleMouseEnter("ubicacion")}
+            onMouseLeave={() => handleMouseLeave("ubicacion")}
+          >
             <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8] group-hover:bg-orange-300">
-            <box-icon name='map' color={isHovered ? '#eeeeee' : '#ffb03b'}></box-icon>
+              <box-icon
+                name="map"
+                color={hoverStates["ubicacion"] ? "#eeeeee" : "#ffb03b"} 
+                className="duration-700"
+              ></box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
               Ubicación:
@@ -34,9 +53,14 @@ export default function Contacto() {
               contacto@example.com
             </p>
           </div>
-          <div className="w-[25%]"  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8]">
-              <box-icon name="time-five" color={isHovered ? '#eeeeee' : '#ffb03b'}></box-icon>
+          <div className="w-[25%] group"
+            onMouseEnter={() => handleMouseEnter("horarios")}
+            onMouseLeave={() => handleMouseLeave("horarios")}>
+            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8] group-hover:bg-orange-300">
+              <box-icon name="time-five" 
+                color={hoverStates["horarios"] ? "#eeeeee" : "#ffb03b"}>
+
+              </box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
               Horarios:
@@ -47,9 +71,14 @@ export default function Contacto() {
               contacto@example.com
             </p>
           </div>
-          <div className="w-[25%]"  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8]">
-              <box-icon name="envelope" color={isHovered ? '#eeeeee' : '#ffb03b'}></box-icon>
+          <div className="w-[25%] group"
+            onMouseEnter={() => handleMouseEnter("email")}
+            onMouseLeave={() => handleMouseLeave("email")}>
+            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8] group-hover:bg-orange-300">
+              <box-icon name="envelope"
+                color={hoverStates['email'] ? "#eeeeee" : "#ffb03b" }>
+
+              </box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
               Email:
@@ -60,9 +89,14 @@ export default function Contacto() {
               contacto@example.com
             </p>
           </div>
-          <div className="w-[25%]"  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8]">
-              <box-icon name="phone" color={isHovered ? '#eeeeee' : '#ffb03b'}></box-icon>
+          <div className="w-[25%] group"
+            onMouseEnter={() => handleMouseEnter("llamanos")}
+            onMouseLeave={() => handleMouseLeave("llamanos")}>
+            <i className="w-10 h-10 rounded-full flex justify-center items-center float-left bg-[#fff6e8] group-hover:bg-orange-300">
+              <box-icon name="phone" 
+                color={hoverStates['llamanos'] ? "#eeeeee" : "#ffb03b"}>
+
+              </box-icon>
             </i>
             <h4 className="ps-12 text-xl font-semibold text-gray-800">
               Llamanos:
