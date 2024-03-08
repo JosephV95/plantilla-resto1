aba8fcf22fe37080d5edc788b46dbf1f9b648b60
JosephV95 <viltejl@gmail.com>
2024-03-08 19:31:31 -0300

    Se esta agrego id para el funcionamiento del nav

--- src/components/GalleriaImg/GalleriaImg.jsx
+++ src/components/GalleriaImg/GalleriaImg.jsx
@@ -18,7 +18,7 @@ export default function GalleriaImg() {
 	}
 
   return (
-    <section className="text-center flex flex-col items-center gap-y-4   my-8 py-3">
+    <section className="text-center flex flex-col items-center gap-y-4   my-8 py-3" id="galeria" >
       <h1 className="text-3xl font-semibold text-[#5f5950]">Algunas fotos de <span className="text-[#ffb03b]">Nuestro Restaurante</span></h1>
       <p className="w-4/5 md:w-3/5 text-lg text-[#68645e]">Ut possimus qui ut temporibus culpa velit eveniet modi omnis est adipisci expedita at voluptas atque vitae autem.</p>
 
