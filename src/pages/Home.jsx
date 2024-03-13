2ae2e1e2e2cf091964f01a0fdf50d6797dc75402
JosephV95 <viltejl@gmail.com>
2024-03-12 23:39:23 -0300

    Se agrego componente del menu comidas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home() {
       <CarouselDelHeader />
       <SobreNosotros />
       <Hero />
-      {/* <MenuDelResto /> */}
+      <MenuDelResto />
 
       <EventosPrivados />
       <GalleriaImg />
