0ce049600e3373f673ec27ce86f0483116a903a1
JosephV95 <viltejl@gmail.com>
2024-03-20 00:18:57 -0300

    Se logro el funcionamiento de active al seleccionar una categoria de comida

--- src/components/MenuDelResto/MenuDelResto.jsx
+++ src/components/MenuDelResto/MenuDelResto.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import ComidaItem from "./ComidaItem";
 import Filterizr from 'filterizr'
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import comidasActions from "../../store/actions/comidasActions";
 
 export default function MenuDelResto() {
@@ -9,11 +9,15 @@ export default function MenuDelResto() {
   const comidasStore = useSelector(store => store.comidasReducer.comidas)
   const comidasRespaldoStore = useSelector(store => store.comidasReducer.comidasRespaldo)
 
+  const [categoriaElegida, setCategoriaElegida] = useState("all")
+
   //* funcion para filtrado, en caso de que se clickee 'todas' se pasa como parametro el backup con todas las comidas
   const filtroParaComidas = (elArray, palabra)=>{
     if (palabra != "all") {
+      setCategoriaElegida(palabra)
       dispatch(comidasActions.filtrar_comidas( {unArray: elArray, categoria: palabra} ))
     } else {
+      setCategoriaElegida("all")
       dispatch(comidasActions.obtener_comidas(comidasRespaldoStore))
     }
   }
@@ -21,6 +25,11 @@ export default function MenuDelResto() {
   useEffect(() => {
     console.log(comidasStore);
   },[]);
+
+  // * effect para verificar la categoria elegida y asi poder apicar el efecto
+  useEffect(()=>{
+    console.log(categoriaElegida);
+  }, [categoriaElegida])
 
   // useEffect(() => {
   //     let filterizr = new Filterizr('.filter-container', options);  
@@ -39,13 +48,13 @@ export default function MenuDelResto() {
 
         <div>
           <ul className="flex flex-row flex-wrap justify-center gap-x-3 gap-y-2 ">
-            <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white" data-filter="all" 
+            <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white ${categoriaElegida =="all"? "bg-[#ffb03b] text-white" :""}`} data-filter="all" 
               onClick={()=> {filtroParaComidas(comidasRespaldoStore, "all")}}>Todas</li>
-            <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white" data-filter="carnes" 
+            <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white ${categoriaElegida =="carnes"? "bg-[#ffb03b] text-white" :""}`} data-filter="carnes" 
               onClick={()=> {filtroParaComidas(comidasRespaldoStore, "carnes")}}>Carnes</li>
-            <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white" data-filter="pastas" 
+            <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white ${categoriaElegida =="pastas"? "bg-[#ffb03b] text-white" :""}`} data-filter="pastas" 
               onClick={()=> {filtroParaComidas(comidasRespaldoStore, "pastas")}}>Pastas</li>
-            <li className="px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white" data-filter="ensaladas" 
+            <li className={`px-4 py-2 cursor-pointer border-2 border-solid border-[#ffb03b] rounded-3xl inline hover:bg-[#ffb03b] hover:text-white ${categoriaElegida =="ensaladas"? "bg-[#ffb03b] text-white" :""}`} data-filter="ensaladas" 
               onClick={()=> {filtroParaComidas(comidasRespaldoStore, "ensaladas")}}>Ensaladas</li>
           </ul>
         </div>
