8a3c528fec14eb36837003fbeb9213768899cea5
JosephV95 <viltejl@gmail.com>
2024-03-26 00:14:21 -0300

    Se quito una clase innecesaria

--- src/components/MenuDelResto/ComidaItem.jsx
+++ src/components/MenuDelResto/ComidaItem.jsx
@@ -2,7 +2,7 @@
 
 export default function ComidaItem({nombre, img, descripcion, precio, categoria}) {
   return (
-    <div className={`w-[100%] lg:w-1/2 flex flex-row justify-center   filtr-item loading`} data-category={categoria}>
+    <div className={`w-[100%] lg:w-1/2 flex flex-row justify-center   filtr-item `} data-category={categoria}>
         <div className="">
             <img src={img} alt={nombre} className="w-14 sm:w-20   object-cover rounded-full" />
         </div>
