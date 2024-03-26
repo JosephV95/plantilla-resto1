56b207f3b306e73bfb5bf4c939e3bffba585e235
JosephV95 <viltejl@gmail.com>
2024-03-26 00:27:36 -0300

    Se cambio ruta de la api

--- src/service/comidasService.js
+++ src/service/comidasService.js
@@ -2,8 +2,8 @@
 export const traerComidas = async()=>{
     try {
         let comidas = []
-        // await fetch('src/utils/comidasApi.json')
-        await fetch('https://foodied-server-nacho-fernandezs-projects.vercel.app/comidas/all')
+        await fetch('src/utils/comidasApi.json')
+        // await fetch('https://foodied-server-nacho-fernandezs-projects.vercel.app/comidas/all')
         .then((res)=> res.json())
         .then( res => comidas = res.comidas)
 
